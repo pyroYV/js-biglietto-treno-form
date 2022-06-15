8a7d29257938eebbd05c6be1c9956f9e76518840
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-15 17:48:15 +0200

    Milestone 2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const userKm = parseInt(prompt('Enter how long is your trip in Km'));
 const userAge = parseInt(prompt ('How old are you?')); */
 const priceKm = 0.26;
 let submit = document.getElementById('submit-btn');
+let reset = document.getElementById('reset-btn');
 
 submit.addEventListener('click',function() {
     const userKm = (document.getElementById('input-km').value);
@@ -11,29 +12,34 @@ submit.addEventListener('click',function() {
     const userName = (document.getElementById('input-name').value);
     const standardPrice = userKm * priceKm;
 
-    if ((userKm < 0) || (userAge < 0)){
-        console.log("error, only positive numbers")
+    if ((userKm < 0) || (userAge <= 0)){
+        document.getElementById('error').classList.remove('d-none');
     } else {
         document.getElementById('result').classList.remove('d-none');
         document.getElementById('user-name').innerHTML = userName;
         document.getElementById('wagon').innerHTML = (Math.floor(Math.random()*10000)+1);
         document.getElementById('cp-code').innerHTML = (Math.floor(Math.random()*100)+1);
+        document.getElementById('error').classList.add('d-none');
 
         if (userAge < 18){
             const discounted = (standardPrice * 85 / 100)
-            console.log("Your ticket's price was " + standardPrice.toFixed(2), "but since you're really young you'll pay " + discounted.toFixed(2));
             document.getElementById('sale-type').innerHTML = "troppo giovane per pagare prezzo intero";
             document.getElementById('sale-price').innerHTML = discounted + "€";
         } else if(userAge >= 65){
             const discounted = standardPrice * 65 / 100
-            console.log("Your price was " + standardPrice.toFixed(2), "but since you're really old you'll pay " + discounted.toFixed(2))
+            document.getElementById('sale-type').innerHTML = "troppo veccho per pagare prezzo intero";
+            document.getElementById('sale-price').innerHTML = discounted + "€";
         } else {
-            console.log("Your ticket's price is" + standardPrice.toFixed(2))
+            document.getElementById('sale-type').innerHTML = "Biglietto standard";
+            document.getElementById('sale-price').innerHTML = standardPrice + "€";
         }
     }  
 })
 
-
+reset.addEventListener('click',function(){
+    document.getElementById('result').classList.add('d-none');
+    document.getElementById('error').classList.add('d-none');
+})
 
 
 
